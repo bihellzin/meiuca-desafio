22c88ab66dcc293a4678eb67ee096b2169a6aa3d
bihellzin <oliva.gabriel@outlook.com>
2024-05-13 03:26:08 -0300

    use stack component to structure the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { CardContent } from "./components/CardContent/CardContent";
-import * as tokens from "./style-dictionary-dist/variables";
+import { Stack } from "./components/Stack/Stack";
+import {
+  FontSizeGiant,
+  SpacingInlineSizeXxs,
+} from "./style-dictionary-dist/variables";
 
 function App() {
   const [articles, setArticles] = useState([]);
@@ -11,29 +15,32 @@ function App() {
       "https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey=6ae9b538ed0b4f9ba18552cc49e29631"
     )
       .then((response) => response.json())
-      .then((response) => setArticles(response.articles));
+      .then(
+        (response) => response.status === "ok" && setArticles(response.articles)
+      );
   }, []);
 
   return (
-    <>
-      <div
+    <Stack>
+      <h1
         style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: tokens.SpacingStackSizeNano,
+          fontSize: FontSizeGiant,
+          textAlign: "center",
+          marginBottom: SpacingInlineSizeXxs,
         }}
       >
-        {articles.map((article, index) => (
-          <CardContent
-            title={article.title.split(" - ")[0]}
-            subtitle={article.author}
-            url={article.url}
-            paragraph={article.description}
-            key={index}
-          ></CardContent>
-        ))}
-      </div>
-    </>
+        Meiuca News
+      </h1>
+      {articles.map((article, index) => (
+        <CardContent
+          title={article.title.split(" - ")[0]}
+          subtitle={article.author}
+          url={article.url}
+          paragraph={article.description}
+          key={index}
+        ></CardContent>
+      ))}
+    </Stack>
   );
 }
 
